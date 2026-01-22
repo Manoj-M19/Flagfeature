231c26dec34fa096dd08aaed78ccc464a4b1d890
Manoj-M19 <manojpavithra2004@gmail.com>
2026-01-22 19:12:34 +0000

    added login route

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,24 +1,27 @@
-import bcrypt from "bcryptjs"
-import  jwt  from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || 'fallback-secret'
+const JWT_SECRET = process.env.JWT_SECRET || "fallback-secret";
 
-export async function hashPassword(password:string):Promise<string>{
-    return bcrypt.hash(password,10)
+export async function hashPassword(password: string): Promise<string> {
+  return bcrypt.hash(password, 10);
 }
 
-export async function verifyPassword(password:string,hash:string):Promise<boolean>{
-  return bcrypt.compare(password,hash)
+export async function verifyPassword(
+  password: string,
+  hash: string,
+): Promise<boolean> {
+  return bcrypt.compare(password, hash);
 }
 
-export function generateToken(userId:string):string {
-    return jwt.sign({ userId},JWT_SECRET,{ expiresIn:'7d'})
+export function generateToken(userId: string): string {
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
 }
 
-export function verifyToken(token:string):{userId:string} |null{
-    try {
-        return jwt.verify(token,JWT_SECRET) as {userId:string}
-    } catch {
-        return null;
-    }
-};
+export function verifyToken(token: string): { userId: string } | null {
+  try {
+    return jwt.verify(token, JWT_SECRET) as { userId: string };
+  } catch {
+    return null;
+  }
+}
