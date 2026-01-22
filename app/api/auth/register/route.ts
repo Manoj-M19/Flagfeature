e8871f52c2bdd5e655128fb175213a3700dd4a3f
Manoj-M19 <manojpavithra2004@gmail.com>
2026-01-22 17:54:00 +0000

    added register route

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,48 @@
+import { generateToken, hashPassword } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function POST (req:NextRequest) {
+    try {
+        const {email,password,name} = await req.json()
+
+        if(!email ||!password) {
+            return NextResponse.json(
+                {error:'Email and password required'},
+                {status:400}
+            )
+        }
+
+        const exists = await prisma.user.findUnique({ where:{email}})
+        if(exists){
+            return NextResponse.json(
+                {error:"User already exists"},
+                { status:400}
+            )
+        }
+
+        const hashedPassowrd = await hashPassword(password)
+        const user = await prisma.user.create({
+            data:{
+                email,
+                password:hashPassword,
+                name:name || null,
+            },
+            select:{
+                id:true,
+                email:true,
+                name:true,
+                role:true,
+            },
+        })
+
+        const token = generateToken(user.id)
+          return NextResponse.json({ user,token},{status:201})
+    } catch (error) {
+        console.error('Registration error:',error)
+        return NextResponse.json(
+            {error:"Internal server error"},
+            {status:500}
+        )
+    }
+}
