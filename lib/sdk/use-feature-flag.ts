2921f12c140339087bf6b4ff51536d937a5c00e5
Manoj-M19 <manojpavithra2004@gmail.com>
2026-02-24 18:43:58 +0000

    Added use feature flags

--- lib/sdk/use-feature-flag.ts
+++ lib/sdk/use-feature-flag.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import type { FlagFeatureClient } from "./client";
-import { fa } from "zod/locales";
 
 export function useFeatureFlags(client:FlagFeatureClient) {
     const [flags,setFlags] = useState<Record<string,boolean>>({})
